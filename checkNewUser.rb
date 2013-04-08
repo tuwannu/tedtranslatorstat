ad97481db6d1c834394915a4ba45d65974972264
Unnawut Leepaisalsuwanna <tuwannu@gmail.com>
2013-04-08 14:42:08 +0700

    Remove sensitive data from source code

--- checkNewUser.rb
+++ checkNewUser.rb
@@ -3,44 +3,33 @@ require 'nokogiri'
 
 startTime = Time.new.getlocal("+07:00")
 
-language_coordinators = [
-	{ :language => 'Thai', :code => 'th', :emails => 'removed@removed.com' },
-	{ :language => 'Polish', :code => 'pl', :emails => 'removed@removed.com' },
-	{ :language => 'French', :code => 'fr', :emails => 'removed@removed.com' },
-	{ :language => 'Urdu', :code => 'ur', :emails => 'removed@removed.com' },
-	{ :language => 'Dutch', :code => 'nl', :emails => 'removed@removed.com' },
-	{ :language => 'Russian', :code => 'ru', :emails => 'removed@removed.com' },
-	{ :language => 'Latvian', :code => 'lv', :emails => 'removed@removed.com' },
-	{ :language => 'Japanese', :code => 'ja', :emails => 'removed@removed.com' },
-#	{ :language => '', :code => '', :emails => '' },
-]
-
 log_text = "Script start executing at #{startTime}\n"
 
-language_coordinators.each do |lc|
-	log_text << "#{Time.new.getlocal("+07:00")}: Start scraping for language: #{lc[:language]}\n"
+LastTranslator.all.each do |lastTranslator|
 
-	lastTranslator = LastTranslator.find_by_for_language(lc[:code])
+	language = lastTranslator.language
+	languageCode = lastTranslator.code
+	lcEmails = lastTranslator.lc_emails
+
+	log_text << "#{Time.new.getlocal("+07:00")}: Start scraping for language: #{language}\n"
 
 	if !lastTranslator then
-		log_text << "#{Time.new.getlocal("+07:00")}: Record for #{lc[:language]} not found. Skipping this language.\n"
+		log_text << "#{Time.new.getlocal("+07:00")}: Record for #{language} not found. Skipping this language.\n"
 		next
 	end
 
-	log_text << "#{Time.new.getlocal("+07:00")}: Last page was: #{lastTranslator.last_page}. Latest Amara ID was: #{lastTranslator.amara_id}.\n"
+	log_text << "#{Time.new.getlocal("+07:00")}: Latest translator for #{language} was: #{lastTranslator.amara_id} on page #{lastTranslator.last_page}.\n"
 
-	page = lastTranslator.last_page.to_i
+	page = lastTranslator.last_page
 	lastPage = false
 	newTranslators = []
 
 	until lastPage do
-		url = "http://www.amara.org/en-gb/teams/ted/members/?lang=#{lc[:code]}&page=#{page}"
+		url = "http://www.amara.org/en-gb/teams/ted/members/?lang=#{languageCode}&page=#{page}"
 
 		begin
 			log_text << "#{Time.new.getlocal("+07:00")}: Fetching: #{url}\n"
 			rawhtml = open(URI.encode(url))
-
-			# rawhtml = File.open("amaratest.html")
 
 			log_text << "#{Time.new.getlocal("+07:00")}: Fetching completed.\n"
 		rescue
@@ -50,7 +39,6 @@ language_coordinators.each do |lc|
 		end
 
 		doc = Nokogiri::HTML.parse(rawhtml)
-		# rawhtml.close
 
 		if doc.css('div.pagination span.next_page.disabled').size > 0 then
 			lastPage = true
@@ -74,11 +62,11 @@ language_coordinators.each do |lc|
 
 	end
 
-	log_text << "#{Time.new.getlocal("+07:00")}: Found #{newTranslators.length} new #{lc[:language]} translators\n"
+	log_text << "#{Time.new.getlocal("+07:00")}: Found #{newTranslators.length} new #{language} translators\n"
 
 	if newTranslators.length > 0 then
-		log_text << "Sending email to #{lc[:emails]} for language #{lc[:language]}.\n"
-		LanguageCoMailer.new_translators_email(lc[:code], lc[:language], lc[:emails], newTranslators).deliver
+		log_text << "Sending email to #{language} LCs (#{lcEmails}).\n"
+		LanguageCoMailer.new_translators_email(languageCode, language, lcEmails, newTranslators).deliver
 
 		t = newTranslators[-1, 1][0]
 		lastTranslator.amara_id = t
@@ -87,7 +75,7 @@ language_coordinators.each do |lc|
 
 		lastTranslator.save
 
-		log_text << "#{Time.new.getlocal("+07:00")}: Last translator for #{lc[:language]} is now: #{lastTranslator.amara_id} on page #{lastTranslator.last_page}\n"
+		log_text << "#{Time.new.getlocal("+07:00")}: Latest translator for #{language} is now: #{lastTranslator.amara_id} on page #{lastTranslator.last_page}\n"
 	end
 
 	sleep 2
@@ -97,8 +85,7 @@ end
 log_text << "#{Time.new.getlocal("+07:00")}: Check new user completed in #{Time.new.getlocal("+07:00")-startTime} seconds.\n"
 
 begin
-	File.open(ENV['OPENSHIFT_REPO_DIR'] + "custom_log/checkNewUser.rb.log", 'a') {|f| f.write(log_text) }	
-	# puts log_text
+	File.open("./custom_log/checkNewUser.rb.log", 'a') {|f| f.write(log_text) }	
 rescue
 	puts "Error logging to file."
 end
