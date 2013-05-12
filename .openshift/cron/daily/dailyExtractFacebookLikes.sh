ce0d0e603082f76e33f238a10d8d4609d2c00efd
Unnawut Leepaisalsuwanna <tuwannu@gmail.com>
2013-05-13 00:49:39 +0700

    Fix cron command for timeout before operation finishes

--- .openshift/cron/daily/dailyExtractFacebookLikes.sh
+++ .openshift/cron/daily/dailyExtractFacebookLikes.sh
@@ -2,4 +2,4 @@
 
 source ~/app-root/data/.bash_profile
 
-cd ${OPENSHIFT_REPO_DIR}; timeout 2000 rails runner -e production dailyExtractFacebookLikes.rb
+cd ${OPENSHIFT_REPO_DIR}; rails runner -e production dailyExtractFacebookLikes.rb
