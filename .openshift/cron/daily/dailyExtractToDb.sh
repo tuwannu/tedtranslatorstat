78d00b4440e766b76252b8ba0dae956c2e31b01c
Unnawut Leepaisalsuwanna <tuwannu@gmail.com>
2013-05-18 00:52:37 +0700

    Fix nohup command

--- .openshift/cron/daily/dailyExtractToDb.sh
+++ .openshift/cron/daily/dailyExtractToDb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup ${OPENSHIFT_REPO_DIR}script/rails runner "${OPENSHIFT_REPO_DIR}dailyExtractToDb.rb"
+nohup ${OPENSHIFT_REPO_DIR}script/rails runner "${OPENSHIFT_REPO_DIR}dailyExtractToDb.rb" &
