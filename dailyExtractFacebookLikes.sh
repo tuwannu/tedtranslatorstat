5784322b5427ed77d91281129ae5a85fb5d992ca
Unnawut Leepaisalsuwanna <tuwannu@gmail.com>
2013-06-22 03:22:51 +0700

    Remove unused files

--- dailyExtractFacebookLikes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/app-root/data/.bash_profile
-
-cd ${OPENSHIFT_REPO_DIR}; nohup rails runner -e production dailyExtractFacebookLikes.rb >/dev/null 2>&1 &
--- dailyExtractToDb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup ${OPENSHIFT_REPO_DIR}script/rails runner "${OPENSHIFT_REPO_DIR}dailyExtractToDb.rb" >/dev/null 2>&1 &
